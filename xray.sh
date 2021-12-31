0e6c58b613e14b0bda37c691cfa7283896b6432e
vincent <vincent0214@qq.com>
2021-12-31 16:05:19 +0800

    增加函数updateNginxPage

--- xray.sh
+++ xray.sh
@@ -718,14 +718,21 @@ EOF
         fi
     fi
     
-    # 修改nginx默认页面
-    rm -rf /usr/share/nginx/html
-    mkdir -p /usr/share/nginx/html
-    wget https://github.com/vincent0214/static_html/releases/download/1.0/static_html.zip -P  /usr/share/nginx/html
-    unzip -o -d /usr/share/nginx/html  /usr/share/nginx/html/static_html.zip
-    rm /usr/share/nginx/html/static_html.zip
-    mv /usr/share/nginx/html/static_html/*  /usr/share/nginx/html/ 
-    rm -r /usr/share/nginx/html/static_html
+    updateNginxPage
+}
+
+# 修改nginx默认页面
+updateNginxPage() {
+    HTML_PATH="/usr/share/nginx/html"
+    STATIC_HTML_URL="https://github.com/vincent0214/static_html/releases/download/1.0/static_html.zip"
+
+    rm -rf ${HTML_PATH}
+    mkdir -p ${HTML_PATH}
+    wget ${STATIC_HTML_URL} -P  ${HTML_PATH}
+    unzip -o -d ${HTML_PATH}  ${HTML_PATH}/static_html.zip
+    rm ${HTML_PATH}/static_html.zip
+    mv ${HTML_PATH}/static_html/*  ${HTML_PATH}/ 
+    rm -rf ${HTML_PATH}/static_html
 }
 
 setSelinux() {
