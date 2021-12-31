6d320597652983e5f8f4859832d8b8c62e3b28c6
vincent <vincent0214@qq.com>
2021-12-31 15:56:59 +0800

    重构代码

--- setNginxDefaultHtml.sh
+++ setNginxDefaultHtml.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-# 下载网页模板到 文件夹/usr/share/nginx/html
-rm -rf /usr/share/nginx/html
-mkdir -p /usr/share/nginx/html
-wget wget https://github.com/vincent0214/static_html/releases/download/1.0/static_html.zip -P  /usr/share/nginx/html
-unzip -o -d /usr/share/nginx/html  /usr/share/nginx/html/static_html.zip
-rm /usr/share/nginx/html/static_html.zip
-mv /usr/share/nginx/html/static_html/*  /usr/share/nginx/html/ 
-rm -r /usr/share/nginx/html/static_html
+HTML_PATH="/tmp/usr/share/nginx/html2"
+STATIC_HTML_URL="https://github.com/vincent0214/static_html/releases/download/1.0/static_html.zip"
+
+# 修改nginx默认页面
+rm -rf ${HTML_PATH}
+mkdir -p ${HTML_PATH}
+wget ${STATIC_HTML_URL} -P  ${HTML_PATH}
+unzip -o -d ${HTML_PATH}  ${HTML_PATH}/static_html.zip
+rm ${HTML_PATH}/static_html.zip
+mv ${HTML_PATH}/static_html/*  ${HTML_PATH}/ 
+rm -rf ${HTML_PATH}/static_html
