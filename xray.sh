740ad7d7971434a1a51cc9a6f771b0e050dea073
vincent <vincent0214@qq.com>
2022-04-23 14:57:03 +0800

    update xray.sh

--- xray.sh
+++ xray.sh
@@ -277,14 +277,14 @@ getData() {
             colorEcho ${BLUE}  " 检测到自有证书，将使用其部署"
             CERT_FILE="/usr/local/etc/xray/${DOMAIN}.pem"
             KEY_FILE="/usr/local/etc/xray/${DOMAIN}.key"
-        else
-            resolve=`curl -sL https://hijk.art/hostip.php?d=${DOMAIN}`
-            res=`echo -n ${resolve} | grep ${IP}`
-            if [[ -z "${res}" ]]; then
-                colorEcho ${BLUE}  "${DOMAIN} 解析结果：${resolve}"
-                colorEcho ${RED}  " 域名未解析到当前服务器IP(${IP})!"
-                exit 1
-            fi
+        # else
+        #     resolve=`curl -sL https://hijk.art/hostip.php?d=${DOMAIN}`
+        #     res=`echo -n ${resolve} | grep ${IP}`
+        #     if [[ -z "${res}" ]]; then
+        #         colorEcho ${BLUE}  "${DOMAIN} 解析结果：${resolve}"
+        #         colorEcho ${RED}  " 域名未解析到当前服务器IP(${IP})!"
+        #         exit 1
+        #     fi
         fi
     fi
 
